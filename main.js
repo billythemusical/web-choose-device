c73aacfdab151d237b6f057aa09b34e19c57ebbb
Billy Bennett <bennett.billy@gmail.com>
2021-02-23 11:23:26 -0500

    revert to example code with path changes

--- main.js
+++ main.js
@@ -1,4 +1,3 @@
-
 /*
 *  Copyright (c) 2015 The WebRTC project authors. All Rights Reserved.
 *
@@ -7,24 +6,15 @@
 *  tree.
 */
 
-"use strict"
+'use strict';
 
-const videoElement = document.querySelector("video")
-const audioInputSelect = document.querySelector("select#audioSource")
-const audioOutputSelect = document.querySelector("select#audioOutput")
-const videoSelect = document.querySelector("select#videoSource")
-const selectors = [audioInputSelect, audioOutputSelect, videoSelect]
+const videoElement = document.querySelector('video');
+const audioInputSelect = document.querySelector('select#audioSource');
+const audioOutputSelect = document.querySelector('select#audioOutput');
+const videoSelect = document.querySelector('select#videoSource');
+const selectors = [audioInputSelect, audioOutputSelect, videoSelect];
 
-audioOutputSelect.disabled = !("sinkId" in HTMLMediaElement.prototype)
-
-function gotDevices(deviceInfos) {  // Handles being called several times to update labels. Preserve values.
-  const values = selectors.map(select => select.value) // Replace each element in array with its own value
-  selectors.forEach(select => {
-    while (select.firstChild) {
-      select.removeChild(select.firstChild)
-    }
-  })
-}
+audioOutputSelect.disabled = !('sinkId' in HTMLMediaElement.prototype);
 
 function gotDevices(deviceInfos) {
   // Handles being called several times to update labels. Preserve values.
@@ -58,59 +48,58 @@ function gotDevices(deviceInfos) {
   });
 }
 
-navigator.mediaDevices.enumerateDevices().then(gotDevices).catch(handleError)
+navigator.mediaDevices.enumerateDevices().then(gotDevices).catch(handleError);
 
 // Attach audio output device to video element using device/sink ID.
-function attachSinkId(element, sinkId)  {
-  if(typeof  element.sinkId !== "undefined") {
+function attachSinkId(element, sinkId) {
+  if (typeof element.sinkId !== 'undefined') {
     element.setSinkId(sinkId)
-      .then(() => {
-        console.log(`Success, audio output device attached: ${sinkId}`)
-      })
-      .catch(error =>{
-        let errorMessage = error
-        if (error.name === "SecurityError") {
-          errorMessage = `You need to use HTTPS for selecting audio output device: ${error}`
-        }
-        console.log(errorMessage)
-        // Jump back to first output device in the list as it"s the default.
-        audioOutputSelect.selectedIndex = 0
-      })
+        .then(() => {
+          console.log(`Success, audio output device attached: ${sinkId}`);
+        })
+        .catch(error => {
+          let errorMessage = error;
+          if (error.name === 'SecurityError') {
+            errorMessage = `You need to use HTTPS for selecting audio output device: ${error}`;
+          }
+          console.error(errorMessage);
+          // Jump back to first output device in the list as it's the default.
+          audioOutputSelect.selectedIndex = 0;
+        });
   } else {
-    console.warn("Browser does not support output device selection.")
+    console.warn('Browser does not support output device selection.');
   }
 }
 
 function changeAudioDestination() {
-  const audioDestination = audioOutputSelect.value
-  attachSinkId(videoElement, audioDestination)
+  const audioDestination = audioOutputSelect.value;
+  attachSinkId(videoElement, audioDestination);
 }
 
 function gotStream(stream) {
-  window.stream = stream // make stream avbl to console
-  videoElement.srcObject = stream
+  window.stream = stream; // make stream available to console
+  videoElement.srcObject = stream;
   // Refresh button list in case labels have become available
-  return navigator.mediaDevices.enumerateDevices()
+  return navigator.mediaDevices.enumerateDevices();
 }
 
 function handleError(error) {
-  console.log("navigator.MediaDevices.getUserMedia error: ", error.message, error.name)
+  console.log('navigator.MediaDevices.getUserMedia error: ', error.message, error.name);
 }
 
 function start() {
   if (window.stream) {
-    window.stream.getTracks().forEach(track =>{
-      track.stop()
-    })
+    window.stream.getTracks().forEach(track => {
+      track.stop();
+    });
   }
-
-  const audioSource = audioInputSelect.value
-  const videoSource = videoSelect.value
+  const audioSource = audioInputSelect.value;
+  const videoSource = videoSelect.value;
   const constraints = {
     audio: {deviceId: audioSource ? {exact: audioSource} : undefined},
-    video: {deivceId: videoSource ? {exact: videoSource} : undefined}
-  }
-  navigator.mediaDevices.getUserMedia(constraints).then(gotStream).then(gotDevices).catch(handleError)
+    video: {deviceId: videoSource ? {exact: videoSource} : undefined}
+  };
+  navigator.mediaDevices.getUserMedia(constraints).then(gotStream).then(gotDevices).catch(handleError);
 }
 
 audioInputSelect.onchange = start;
